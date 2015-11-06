2789749a3ecb61465e484edc524d26c2d5298dde
Matt Young <matt@fansz.com>
2015-11-06 11:39:01 +0000

    Added DB!

--- Api/HomeHandler.go
+++ Api/HomeHandler.go
@@ -2,9 +2,13 @@ package Api
 
 import (
 	"net/http"
+	"log"
 )
 
 func HomeHandler() http.Handler{
+
+	log.Print("hi")
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 
 		w.Write([]byte("ok"))
