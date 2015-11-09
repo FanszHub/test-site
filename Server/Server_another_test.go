b01968339b4ca2f42a27fe720e9063c89fd45b3f
Matt Young <matt@fansz.com>
2015-11-09 16:11:38 +0000

    Added DB!

--- Server/Server_another_test.go
+++ Server/Server_another_test.go
@@ -5,6 +5,9 @@ import (
 	. "github.com/onsi/gomega"
 	"strings"
 	"net/http"
+	"io/ioutil"
+	"github.com/FanszHub/test-site/Models"
+	"encoding/json"
 )
 
 var _ = Describe("UserRegister", func() {
@@ -22,5 +25,21 @@ var _ = Describe("UserRegister", func() {
 		res, _ := http.DefaultClient.Do(request)
 
 		Expect(res.StatusCode).To(Equal(200))
+
+		request, _ = http.NewRequest("GET", "http://localhost:3131/users", nil) //Create request with JSON body
+
+		res, _ = http.DefaultClient.Do(request)
+
+		Expect(res.StatusCode).To(Equal(200))
+
+		content, _ := ioutil.ReadAll(res.Body)
+
+		var users []Models.User
+
+		json.Unmarshal(content,&users)
+
+		Expect(len(users)).To(Equal(1))
+		Expect(users[0].Username).To(Equal("dennis"))
+
 	})
 })
